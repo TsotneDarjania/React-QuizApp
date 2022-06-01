55fad10005058eb5b6219e197622a2115c68b8b4
DefaultUserName123 <98889556+TsotneDarjania@users.noreply.github.com>
2022-06-01 22:04:15 +0400

    add contexts

--- quizz-app/src/App.js
+++ quizz-app/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+import React, {useState} from "react";
+import MainMenu from './Components/MainMenu';
+import Quiz from './Components/Quiz';
+import EndScreen from './Components/EndScreen';
+
 function App() {
+
+  const [gameState,setGameState] = useState("menu");
+
+
   return (
     <div className="App">
-     
+     <h1>Quiz App</h1>
+
+     {gameState === "menu" && <MainMenu />}
+     {gameState === "quiz" && <Quiz />}
+     {gameState === "endScreen" && <EndScreen />}
+
     </div>
   );
 }
---
+++ quizz-app/src/Components/MainMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MainMenu() {
+  return (
+    <div className='menu'>
+
+        <button> Start Quiz</button>
+    </div>
+  )
+}
+
